5030af677a39f4e35b9da60a31098e22eb1d14da
uanoob <usnet@ukr.net>
2018-09-10 13:47:59 +0300

    Add changing colors to section -> intro

--- source/js/app.js
+++ source/js/app.js
@@ -1,1 +1,13 @@
-console.log("js ok");
+const intro = document.querySelector('.intro');
+const subtitle = document.querySelector('.intro__subtitle');
+
+function changeColors() {
+  intro.classList.toggle('intro--white');
+  subtitle.classList.toggle('intro__subtitle--white');
+}
+
+function timer() {
+  setInterval(changeColors, 4000);
+}
+
+document.addEventListener('DOMContentLoaded', timer());
