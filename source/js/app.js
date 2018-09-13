f01962e5dddbf7890900bb113604eed00275f3c5
uanoob <usnet@ukr.net>
2018-09-13 16:47:19 +0300

    Add slider -> all-products

--- source/js/app.js
+++ source/js/app.js
@@ -11,3 +11,41 @@ function timer() {
 }
 
 document.addEventListener('DOMContentLoaded', timer());
+
+// Slider
+const prevBtn = document.querySelector('.btn__slider--prev');
+const nextBtn = document.querySelector('.btn__slider--next');
+const slides = document.querySelectorAll('.slide-item');
+let slideIndex = 2;
+
+prevBtn.addEventListener('click', function() {
+  moveLeft();
+});
+nextBtn.addEventListener('click', function() {
+  moveRight();
+});
+
+function moveLeft() {
+  showSlides((slideIndex -= 1));
+}
+
+function moveRight() {
+  showSlides((slideIndex += 1));
+}
+
+function showSlides(n) {
+  let i;
+  if (n > slides.length) {
+    slideIndex = 2;
+  }
+  if (n < 2) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  slides[slideIndex - 2].style.display = 'block';
+}
+
+showSlides(slideIndex);
